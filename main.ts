3d82d199cdcbaed2809bf146f7175b87d546c0df
neelerau <72865700+neelerau@users.noreply.github.com>
2022-02-03 23:22:21 +0100

    settings

--- main.ts
+++ main.ts
@@ -1,30 +1,27 @@
 namespace Dönerladen {
 
-
     export let crc2: CanvasRenderingContext2D;
     export let storesimulation: Doenerladen;
 
-
-    window.addEventListener("load", handleLoad);
+    window.addEventListener("load", handleLoad); 
 
     function handleLoad(): void {
 
-        let canvas: HTMLCanvasElement | null = document.querySelector("canvas")!;
-        if (!canvas)
-        return;
-        crc2 = <CanvasRenderingContext2D>canvas.getContext("2d")!;
- 
-
+            let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
+            if (!canvas)
+                return;
+            crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
     }
+
 
     const ingrediens: Element = (document.getElementById("ingrediens") as HTMLInputElement);
     const landingPage: Element = (document.getElementById("settings")as HTMLInputElement);
     const startbutton: Element = (document.getElementById("startbutton")as HTMLInputElement);
-    
+
     startbutton.addEventListener("click", function (): void {
         startSimulation();
     });
-
+    
     function startSimulation(): void {
         landingPage.classList.add("is-hidden");
         ingrediens.classList.remove("is-hidden");
